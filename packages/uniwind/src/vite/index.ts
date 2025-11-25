4c7a5483764f09dd7bd9a7ca960e38da6b02d385
burakgormek <burak.gormek@gmail.com>
2025-11-25 23:46:13 +0300

    chore: format

--- packages/uniwind/src/vite/index.ts
+++ packages/uniwind/src/vite/index.ts
@@ -1,36 +1,36 @@
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import type { Plugin } from "vite";
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import type { Plugin } from 'vite'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 const componentPath = path.resolve(
-  __dirname,
-  "../module/components/index.web.js"
-);
+    __dirname,
+    '../module/components/index.web.js',
+)
 
 export const uniwind = (): Plugin => {
-  return {
-    name: "uniwind",
-    config: () => ({
-      resolve: {
-        alias: [
-          {
-            find: /^react-native$/,
-            replacement: componentPath,
-            customResolver: {
-              resolveId(_, importer) {
-                // Check if import comes from uniwind
-                if (importer?.includes("node_modules/uniwind") === true) {
-                  return this.resolve("react-native-web");
-                }
+    return {
+        name: 'uniwind',
+        config: () => ({
+            resolve: {
+                alias: [
+                    {
+                        find: /^react-native$/,
+                        replacement: componentPath,
+                        customResolver: {
+                            resolveId(_, importer) {
+                                // Check if import comes from uniwind
+                                if (importer?.includes('node_modules/uniwind') === true) {
+                                    return this.resolve('react-native-web')
+                                }
 
-                return componentPath;
-              },
+                                return componentPath
+                            },
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-    }),
-  };
-};
+        }),
+    }
+}
