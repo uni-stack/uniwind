0f743eeb2a606bd3ab00b74692d4060846992164
Hubert Bieszczad <brentlokk@gmail.com>
2025-12-01 11:11:47 +0100

    chore: vite example add min height

--- apps/vite-example/src/App.tsx
+++ apps/vite-example/src/App.tsx
@@ -6,7 +6,7 @@ import {
 
 function App() {
     return (
-        <View className="flex-1 bg-background justify-center items-center">
+        <View className="flex-1 min-h-screen bg-background justify-center items-center">
             <Text className="text-3xl font-bold text-black dark:text-white">
                 Uniwind working in Vite!
             </Text>
