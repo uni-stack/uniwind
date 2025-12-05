fdf0b49289f78496521df1fabb54729404fd219e
Hubert Bieszczad <brentlokk@gmail.com>
2025-12-05 11:52:15 +0100

    chore: minor corrections

--- packages/uniwind/src/css/themes.ts
+++ packages/uniwind/src/css/themes.ts
@@ -2,6 +2,7 @@ import { compile } from '@tailwindcss/node'
 import fs from 'fs'
 import { transform } from 'lightningcss'
 import path from 'path'
+import { name } from '../../package.json'
 import { Logger } from '../metro/logger'
 
 const readFileSafe = (filePath: string) => {
@@ -15,7 +16,7 @@ const readFileSafe = (filePath: string) => {
 const isExcludedDependency = (url: string) =>
     [
         url === 'tailwindcss',
-        url === 'uniwind',
+        url === name,
         url.includes('node_modules/tailwindcss'),
         url.includes('node_modules/@tailwindcss'),
         url.includes('node_modules/uniwind'),
---
+++ packages/uniwind/src/vite/vite.ts
@@ -0,0 +1,85 @@
+import { normalizePath } from '@tailwindcss/node'
+import path from 'path'
+import type { Plugin } from 'vite'
+import { name as UNIWIND_PACKAGE_NAME } from '../../package.json'
+import { buildCSS } from '../css'
+import { processFunctions } from '../css/processFunctions'
+import { uniq } from '../metro/utils'
+import { buildDtsFile } from '../utils/buildDtsFile'
+import { stringifyThemes } from '../utils/stringifyThemes'
+
+type UniwindConfig = {
+    cssEntryFile: string
+    extraThemes?: Array<string>
+    dtsFile?: string
+}
+
+const componentPath = path.resolve(
+    import.meta.dirname,
+    '../module/components/web/index.js',
+)
+
+export const uniwind = ({
+    cssEntryFile,
+    extraThemes,
+    dtsFile = 'uniwind-types.d.ts',
+}: UniwindConfig): Plugin => {
+    const themes = uniq([
+        'light',
+        'dark',
+        ...(extraThemes ?? []),
+    ])
+    const stringifiedThemes = stringifyThemes(themes)
+
+    return {
+        name: 'uniwind',
+        config: () => ({
+            css: {
+                transformer: 'lightningcss',
+                lightningcss: {
+                    visitor: {
+                        Function: processFunctions,
+                    },
+                },
+            },
+            optimizeDeps: {
+                exclude: [UNIWIND_PACKAGE_NAME, 'react-native'],
+            },
+            resolve: {
+                alias: [
+                    {
+                        find: /^react-native$/,
+                        replacement: componentPath,
+                        customResolver: {
+                            resolveId(_, importer) {
+                                // Check if import comes from uniwind
+                                if (importer !== undefined && normalizePath(importer).includes(`${UNIWIND_PACKAGE_NAME}/dist`)) {
+                                    return this.resolve('react-native-web')
+                                }
+
+                                return componentPath
+                            },
+                        },
+                    },
+                ],
+            },
+        }),
+        transform: (code, id) => {
+            const normalizedId = normalizePath(id)
+
+            if (normalizedId.includes(`${UNIWIND_PACKAGE_NAME}/dist`) && normalizedId.includes('config/config.js')) {
+                return {
+                    code: `${code}Uniwind.__reinit(() => ({}), ${stringifiedThemes})`,
+                }
+            }
+        },
+        buildStart: async () => {
+            await buildCSS(themes, cssEntryFile)
+            buildDtsFile(dtsFile, stringifiedThemes)
+        },
+        generateBundle: async () => {
+            await buildCSS(themes, cssEntryFile)
+            buildDtsFile(dtsFile, stringifiedThemes)
+        },
+    }
+}
