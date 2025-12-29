a146b195e22db8a16349d32b570befb4647dce71
Hubert Bieszczad <brentlokk@gmail.com>
2025-12-29 16:38:27 +0100

    test: safe area insets

--- packages/uniwind/tests/setup.ts
+++ packages/uniwind/tests/setup.ts
@@ -1,7 +1,11 @@
 import { readFileSync } from 'fs'
 import { resolve } from 'path'
+import { Dimensions } from 'react-native'
 import { compileVirtual } from '../src/metro/compileVirtual'
 import { Platform } from '../src/metro/types'
+import { SAFE_AREA_INSET_BOTTOM, SAFE_AREA_INSET_TOP, SCREEN_HEIGHT, SCREEN_WIDTH } from './consts'
+
+jest.spyOn(Dimensions, 'get').mockReturnValue({ width: SCREEN_WIDTH, height: SCREEN_HEIGHT, scale: 1, fontScale: 1 })
 
 beforeAll(async () => {
     const cssPath = resolve('./tests/test.css')
@@ -18,6 +22,12 @@ beforeAll(async () => {
     eval(
         `const { Uniwind } = require('../src/core/config/config.native');
         Uniwind.__reinit(rt => ${virtualCode}, ['light', 'dark']);
+            Uniwind.updateInsets({
+            top: ${SAFE_AREA_INSET_TOP},
+            left: 0,
+            bottom: ${SAFE_AREA_INSET_BOTTOM},
+            right: 0,
+        });
     `,
     )
 })
