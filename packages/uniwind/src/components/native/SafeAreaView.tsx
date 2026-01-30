1e23380eb9f7e363b00dfa4a6647f6cdc4f23831
contra <yo@contra.io>
2026-01-30 13:52:11 -0700

    fix: use react-native-safe-area-context instead of deprecated SafeAreaView
    SafeAreaView from react-native has been deprecated and will be removed
    in a future release. This change updates both native and web components
    to import from the recommended react-native-safe-area-context package.
    
    This also fixes a development warning that appears because React Fast
    Refresh enumerates all exports, triggering the deprecation getter even
    when SafeAreaView is not directly used.
    
    Added react-native-safe-area-context as a peer dependency.

--- packages/uniwind/src/components/native/SafeAreaView.tsx
+++ packages/uniwind/src/components/native/SafeAreaView.tsx
@@ -1,8 +1,8 @@
-import { SafeAreaView as RNSafeAreaView, ViewProps } from 'react-native'
+import { SafeAreaView as RNSafeAreaView, type SafeAreaViewProps } from 'react-native-safe-area-context'
 import { copyComponentProperties } from '../utils'
 import { useStyle } from './useStyle'
 
-export const SafeAreaView = copyComponentProperties(RNSafeAreaView, (props: ViewProps) => {
+export const SafeAreaView = copyComponentProperties(RNSafeAreaView, (props: SafeAreaViewProps) => {
     const style = useStyle(props.className, props)
 
     return (
