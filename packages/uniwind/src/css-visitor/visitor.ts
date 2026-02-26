5d51b364a0f5e335372e64716970468d87b41b77
Hubert Bieszczad <48803618+Brentlok@users.noreply.github.com>
2026-02-26 09:47:24 +0100

    chore: fix cleanup css visitor on hot reload (#412)

--- packages/uniwind/src/css-visitor/visitor.ts
+++ packages/uniwind/src/css-visitor/visitor.ts
@@ -5,9 +5,16 @@ import { RuleVisitor } from './rule-visitor'
 export class UniwindCSSVisitor implements Visitor<CustomAtRules> {
     Function: Visitor<CustomAtRules>['Function']
     Rule: Visitor<CustomAtRules>['Rule']
+    StyleSheet: Visitor<CustomAtRules>['StyleSheet']
 
     constructor(private readonly themes: Array<string>) {
+        const ruleVisitor = new RuleVisitor(this.themes)
+
         this.Function = new FunctionVisitor()
-        this.Rule = new RuleVisitor(this.themes)
+        this.Rule = ruleVisitor
+
+        this.StyleSheet = () => {
+            ruleVisitor.cleanup()
+        }
     }
 }
