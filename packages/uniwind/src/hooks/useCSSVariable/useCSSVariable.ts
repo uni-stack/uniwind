6aa428a0cc6f67160b244e77f53f51ae66ff19c7
Hubert Bieszczad <brentlokk@gmail.com>
2026-04-28 11:05:29 +0200

    feat: getCSSVariable function to get css variable outside of react

--- packages/uniwind/src/hooks/useCSSVariable/useCSSVariable.ts
+++ packages/uniwind/src/hooks/useCSSVariable/useCSSVariable.ts
@@ -7,11 +7,6 @@ import { UniwindContextType } from '../../core/types'
 import { StyleDependency } from '../../types'
 import { getVariableValue } from './getVariableValue'
 
-const getValue = (name: string | Array<string>, uniwindContext: UniwindContextType) =>
-    Array.isArray(name)
-        ? name.map(name => getVariableValue(name, uniwindContext))
-        : getVariableValue(name, uniwindContext)
-
 let warned = false
 
 const logDevError = (name: string) => {
@@ -21,53 +16,10 @@ const logDevError = (name: string) => {
     )
 }
 
-type IsGenericNumber<T> = T & 0 extends -1 ? false : true
-type CreateArray<N extends number, Value, TAcc extends Array<Value> = []> = TAcc['length'] extends N ? TAcc : CreateArray<N, Value, [...TAcc, Value]>
-
-type UseCSSVariable = {
-    (name: string): string | number | undefined
-    <const T extends Array<string>>(
-        names: T,
-    ): IsGenericNumber<T['length']> extends true ? Array<string | number | undefined> : CreateArray<T['length'], string | number | undefined>
-}
-
-/**
- * A hook that returns the value of a CSS variable.
- * @param name Name / Array of names of the CSS variable.
- * @returns Value / Values of the CSS variable. On web it is always a string (1rem, #ff0000, etc.), but on native it can be a string or a number (16px, #ff0000)
- */
-export const useCSSVariable: UseCSSVariable = (name: string | Array<string>) => {
-    const uniwindContext = useUniwindContext()
-    const [value, setValue] = useState(getValue(name, uniwindContext))
-    const nameRef = useRef(name)
-
-    useLayoutEffect(() => {
-        if (Array.isArray(name) && Array.isArray(nameRef.current)) {
-            if (arrayEquals(name, nameRef.current)) {
-                return
-            }
-
-            setValue(getValue(name, uniwindContext))
-            nameRef.current = name
-
-            return
-        }
-
-        if (name !== nameRef.current) {
-            setValue(getValue(name, uniwindContext))
-            nameRef.current = name
-        }
-    }, [name])
-
-    useLayoutEffect(() => {
-        const updateValue = () => setValue(getValue(nameRef.current, uniwindContext))
-        const dispose = UniwindListener.subscribe(
-            updateValue,
-            [StyleDependency.Theme, StyleDependency.Variables],
-        )
-
-        return dispose
-    }, [uniwindContext])
+export const getCSSVariable = (name: string | Array<string>, uniwindContext: UniwindContextType) => {
+    const value = Array.isArray(name)
+        ? name.map(name => getVariableValue(name, uniwindContext))
+        : getVariableValue(name, uniwindContext)
 
     if (Array.isArray(value)) {
         value.forEach((val, index) => {
@@ -80,6 +32,57 @@ export const useCSSVariable: UseCSSVariable = (name: string | Array<string>) => 
     if (value === undefined && __DEV__ && !warned) {
         logDevError(name as string)
     }
+
+    return value
+}
+
+type IsGenericNumber<T> = T & 0 extends -1 ? false : true
+type CreateArray<N extends number, Value, TAcc extends Array<Value> = []> = TAcc['length'] extends N ? TAcc : CreateArray<N, Value, [...TAcc, Value]>
+
+export type GetCSSVariable = {
+    (name: string): string | number | undefined
+    <const T extends Array<string>>(
+        names: T,
+    ): IsGenericNumber<T['length']> extends true ? Array<string | number | undefined> : CreateArray<T['length'], string | number | undefined>
+}
+
+/**
+ * A hook that returns the value of a CSS variable.
+ * @param name Name / Array of names of the CSS variable.
+ * @returns Value / Values of the CSS variable. On web it is always a string (1rem, #ff0000, etc.), but on native it can be a string or a number (16px, #ff0000)
+ */
+export const useCSSVariable: GetCSSVariable = (name: string | Array<string>) => {
+    const uniwindContext = useUniwindContext()
+    const [value, setValue] = useState(getCSSVariable(name, uniwindContext))
+    const nameRef = useRef(name)
+
+    useLayoutEffect(() => {
+        if (Array.isArray(name) && Array.isArray(nameRef.current)) {
+            if (arrayEquals(name, nameRef.current)) {
+                return
+            }
+
+            setValue(getCSSVariable(name, uniwindContext))
+            nameRef.current = name
+
+            return
+        }
+
+        if (name !== nameRef.current) {
+            setValue(getCSSVariable(name, uniwindContext))
+            nameRef.current = name
+        }
+    }, [name])
+
+    useLayoutEffect(() => {
+        const updateValue = () => setValue(getCSSVariable(nameRef.current, uniwindContext))
+        const dispose = UniwindListener.subscribe(
+            updateValue,
+            [StyleDependency.Theme, StyleDependency.Variables],
+        )
+
+        return dispose
+    }, [uniwindContext])
 
     return value as never
 }
